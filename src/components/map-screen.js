a496a5c18824f2f7385f00c35a81938ffdcc51bf
Michael Brown <brownieboy@gmail.com>
2019-03-31 18:08:50 +1100

    Map full screen on iOS

--- src/components/map-screen.js
+++ src/components/map-screen.js
@@ -5,26 +5,55 @@
 
 import React, { Component } from "react";
 import MapView from "react-native-maps";
+import { View } from "react-native";
+import {
+  Body,
+  Container,
+  Content,
+  Header,
+  Left,
+  Right,
+  Title
+} from "native-base";
+import appColours from "../styles/appColours.js";
 
-import { Text, View } from "react-native";
 class MapScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>App Map Screen</Text>
-        <MapView
-          initialRegion={{
-            latitude: -33.797474,
-            longitude: 151.286902,
-            // latitudeDelta: 0.0922,
-            // longitudeDelta: 0.0421
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.015
+      <Container>
+        <Header
+          style={{
+            backgroundColor: appColours.panelBackgroundColor
           }}
-          width={300}
-          height={300}
-        />
-      </View>
+        >
+          <Left />
+          <Body style={{ flex: 11 }}>
+            <Title
+              style={{
+                color: appColours.panelTextColor
+              }}
+            >
+              Map Search
+            </Title>
+          </Body>
+          <Right />
+        </Header>
+        {/* <Content> */}
+        <View style={{ flex: 1 }}>
+          <MapView
+            initialRegion={{
+              latitude: -33.797474,
+              longitude: 151.286902,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.015
+            }}
+            // width={300}
+            // height={300}
+            style={{ flex: 1 }}
+          />
+        </View>
+        {/* </Content> */}
+      </Container>
     );
   }
 }
