76fab4e27340174fba81ee031b862892e6e5b342
Michael Brown <brownieboy@gmail.com>
2019-04-02 21:26:01 +1100

    x32 icons

--- src/constants/map-icons.js
+++ src/constants/map-icons.js
@@ -1,6 +1,6 @@
-import pintIcon from "../../resources/map-icons/pint-of-beer-128.png";
-import cocktailIcon from "../../resources/map-icons/cocktail-128.png";
-import burgerIcon from "../../resources/map-icons/burger-128.png";
+import pintIcon from "../../resources/map-icons/pint-of-beer-32.png";
+import cocktailIcon from "../../resources/map-icons/margarita-yellow-32.png";
+import burgerIcon from "../../resources/map-icons/burger-32.png";
 
 const mapIcons = {
   beer: pintIcon,
