624bc1a091a1e2386cd13677f9130398517c16f9
Michael Brown <brownieboy@gmail.com>
2019-04-13 09:57:24 +1000

    Sailing icon for skiffie

--- src/constants/map-icons.js
+++ src/constants/map-icons.js
@@ -1,11 +1,13 @@
 import pintIcon from "../../resources/map-icons/beer-128.png";
 import cocktailIcon from "../../resources/map-icons/cocktail-128.png";
 import burgerIcon from "../../resources/map-icons/burger-128.png";
+import sailingIcon from "../../resources/map-icons/boat-128.png"
 
 const mapIcons = {
   beer: pintIcon,
   cocktail: cocktailIcon,
-  burger: burgerIcon
+  burger: burgerIcon,
+  sailing: sailingIcon
 };
 
 export default mapIcons;
