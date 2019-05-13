4c08a2e1f854c92ff7fcf692ad59174cca6dcd57
Michael Brown <brownieboy@gmail.com>
2019-05-13 22:24:58 +1000

    Map icons to constants

--- src/constants/map-icons.js
+++ src/constants/map-icons.js
@@ -2,20 +2,25 @@ import pintIcon from "../../resources/map-icons/beer-128.png";
 import cocktailIcon from "../../resources/map-icons/cocktail-128.png";
 import burgerIcon from "../../resources/map-icons/burger-128.png";
 import sailingIcon from "../../resources/map-icons/boat-128.png";
+import fishChipsIcon from "../../resouces/map-icons/fish-and-chips-128.png";
 import pintIconMono from "../../resources/map-icons/beer-128-mono.png";
 import cocktailIconMono from "../../resources/map-icons/cocktail-128-mono.png";
 import burgerIconMono from "../../resources/map-icons/burger-128-mono.png";
 import sailingIconMono from "../../resources/map-icons/boat-128-mono.png";
+import fishChipsIconMono from "../../resouces/map-icons/fish-and-chips-128-Mono.png";
+
 
 const mapIcons = {
   beer: pintIcon,
   cocktail: cocktailIcon,
   burger: burgerIcon,
   sailing: sailingIcon,
+  fishAndChips: fishChipsIcon,
   beerMono: pintIconMono,
   cocktailMono: cocktailIconMono,
   burgerMono: burgerIconMono,
-  sailingMono: sailingIconMono
+  sailingMono: sailingIconMono,
+  fishAndChipsMono: fishChipsIconMono
 };
 
 export default mapIcons;
