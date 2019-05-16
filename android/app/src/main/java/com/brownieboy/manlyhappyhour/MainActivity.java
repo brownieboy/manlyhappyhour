67bee5b666a38550a9035bb4fc0f5940081d07ca
Michael Brown <brownieboy@gmail.com>
2019-05-16 13:06:03 +1000

    Initial Android splash screen

--- android/app/src/main/java/com/brownieboy/manlyhappyhour/MainActivity.java
+++ android/app/src/main/java/com/brownieboy/manlyhappyhour/MainActivity.java
@@ -4,9 +4,32 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Context;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
-
+    class CustomReactActivityDelegate extends ReactActivityDelegate {
+        class CustomReactRootView extends ReactRootView {
+            public CustomReactRootView(Context context) {
+                super(context);
+            }
+            @Override
+            public void onViewAdded(View child) {
+                super.onViewAdded(child);
+                Log.d("React views started to appear", "Static js code has already run");
+            }
+        }
+        private Activity currentActivity;
+        public CustomReactActivityDelegate(Activity activity, String mainComponentName) {
+            super(activity, mainComponentName);
+            currentActivity = activity;
+        }
+        protected ReactRootView createRootView() {
+            return new CustomReactRootView(currentActivity);
+        }
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
